ef123cb8865885666a4f6f2f594248400f8eca26
kukuh83 <kukuh83@gmail.com>
2020-05-12 20:12:57 +0700

    tugas 8 dan 9

---
+++ Survival Game Engine/Assets/SampleScenes/Scripts/PlayerFolow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFolow : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform player;
+	public Vector3 offset;
+	// Use this for initialization
+	void Start () {
+
+	}
+	// Update is called once per frame
+	void Update () {
+		transform.position = player.position + offset;
+	}
+}
