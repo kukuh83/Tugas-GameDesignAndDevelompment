78192bedab2fc84474eb784dd2d1c627733edbf9
kukuh83 <kukuh83@gmail.com>
2020-06-03 19:15:21 +0700

    tugas 10

---
+++ Survival Game Engine/Assets/Script/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	[SerializeField]
+	float _secondsToWin = 180.0f;
+
+	static bool _hasPlayerWon = false;
+	public static bool HasPlayerWon{
+		get{ return _hasPlayerWon; }
+	}
+	PlayerHealth _playerHealth;
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("Checkwin", _secondsToWin);
+
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+		_playerHealth = player.GetComponent<PlayerHealth> ();
+	}
+
+	void Checkwin(){
+		if (!_playerHealth.isDeath()){
+			_hasPlayerWon = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Survival Game Engine/Assets/Script/Health.cs
+++ Survival Game Engine/Assets/Script/Health.cs
@@ -6,26 +6,44 @@ public class Health : MonoBehaviour {
 	[SerializeField]
 	int _maximumHealth = 100;
 	int _currentHealth = 0;
+	Renderer _renderer;
+
+	public bool isDeath(){
+		return _currentHealth <= 0;
+	}
+
+	PlayerStats _playerStats;
+
 	// Use this for initialization
 	void Start () {
+		_renderer = GetComponentInChildren<Renderer> ();
 		_currentHealth = _maximumHealth;
-		GetComponent<Health> ().Damage (100);
+
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+		_playerStats = player.GetComponent<PlayerStats>();		
 	}
+	
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (isDeath() && !_renderer.isVisible){
+			Destroy(gameObject);
+		}
 	}
 	public void Damage(int damageValue){
 		_currentHealth -= damageValue;
 		if (_currentHealth <= 0){
-			// Destroy (gameObject);
 			Animation anim = GetComponentInChildren<Animation> ();
 			anim.Stop ();
 
-			Destroy (GetComponent<PlayerMovement> ());
-			Destroy (GetComponent<PlayerAnimation> ());
+			_playerStats.ZombieKilled++;
+			EnemySpawnManager.onEnemyDeath ();
+			Destroy (GetComponent<EnemyMovement> ());
+			Destroy (GetComponent<EnemyAttack> ());
+			Destroy (GetComponent<CharacterController> ());
+			Destroy (gameObject, 8.0f);
 
+			EnemySpawnManager.onEnemyDeath ();
 			Destroy (GetComponent<EnemyMovement> ());
 			Destroy (GetComponent<CharacterController> ());
 
